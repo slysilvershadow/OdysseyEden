6972c0e19dc00e0ff9b348c82fddaf8f26c83cb7
D <littleemoroyalpain@gmail.com>
2024-11-04 14:46:33 +0000

    required

--- requirements.py
+++ requirements.py
@@ -6,10 +6,37 @@ spriteworld
 pillow 
 sckit-learn
 six
-dm-env
-absl-py
-enum34
 matplotlib
 mock
 imageio
 mss
+namemaker
+
+absl-py         2.1.0
+contourpy       1.3.0
+cycler          0.12.1
+dm-env          1.6
+dm-tree         0.1.8
+enum34          1.1.10
+fonttools       4.54.1
+imageio         2.36.0
+joblib          1.4.2
+kiwisolver      1.4.7
+matplotlib      3.9.2
+mock            5.1.0
+moog-games      1.4
+mss             9.0.2
+namemaker       1.2
+numpy           2.1.3
+packaging       24.1
+pillow          11.0.0
+pip             24.3.1
+pygame          2.6.1
+pyparsing       3.2.0
+python-dateutil 2.9.0.post0
+scikit-learn    1.5.2
+scipy           1.14.1
+setuptools      75.1.0
+six             1.16.0
+spriteworld     1.0.2
+threadpoolctl   3.5.0
